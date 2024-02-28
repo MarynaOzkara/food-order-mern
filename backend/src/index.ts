31f2c412b00ef1696e3765d51230a4d4a7a88792
MarynaOzkara <ozkaramaryna@gmail.com>
2024-02-28 13:32:18 +0200

    connertion to mongodb database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
-
+mongoose.connect(process.env.MONGODB_CONNECTION as string).then(() => console.log("Conneced to MongoDB database!"));
 const app = express();
 app.use(express.json());
 app.use(cors());
